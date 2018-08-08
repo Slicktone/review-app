65b5ee5a93a316b2b145c12c3ae89855444940e6
Tony Clayton <tclaytony23@gmail.com>
2018-08-07 18:28:59 -0700

    components added

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            // stuff in here
+            <div className="about">
+            <a id="about" name="about"></a>
+                <h1> This is a Test About Me! </h1>
+                <div className="about-container">
+                    <p> Lorem Ipsum Stuff! </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default About;
